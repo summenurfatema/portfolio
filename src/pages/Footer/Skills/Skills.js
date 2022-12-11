80a16e3010ad56f265f91a5c1e98dbfd5e2674e0
Fatema04 <shadrinmoni15@gmail.com>
2022-12-11 22:35:57 +0600

    complete

--- src/pages/Footer/Skills/Skills.js
+++ src/pages/Footer/Skills/Skills.js
@@ -10,52 +10,52 @@ const Skills = () => {
 
                 <div className=''>
                     <p className='text-xl mb-2'>HTML5</p>
-                    <input type="range" min="0" max="100" defaultValue="100" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="100" className="range w-80 md:w-96 range-primary" />
                 </div>
 
                 <div className=''>
                     <p className='text-xl mb-2'>CSS</p>
-                    <input type="range" min="0" max="100" defaultValue="100" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="100" className="range w-80 md:w-96 range-primary" />
                 </div>
 
                 <div className=''>
                     <p className='text-xl mb-2'>JavaScript</p>
-                    <input type="range" min="0" max="100" defaultValue="95" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="95" className="range w-80 md:w-96 range-primary" />
                 </div>
 
                 <div className=''>
                     <p className='text-xl mb-2'>React</p>
-                    <input type="range" min="0" max="100" defaultValue="98" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="98" className="range w-80 md:w-96 range-primary" />
                 </div>
                 <div className=''>
                     <p className='text-xl mb-2'>BootStrap</p>
-                    <input type="range" min="0" max="100" defaultValue="100" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="100" className="range w-80 md:w-96 range-primary" />
                 </div>
 
                 <div className=''>
                     <p className='text-xl mb-2'>Tailwind CSS</p>
-                    <input type="range" min="0" max="100" defaultValue="100" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="100" className="range w-80 md:w-96 range-primary" />
                 </div>
                 <div className=''>
                     <p className='text-xl mb-2'>FireBase</p>
-                    <input type="range" min="0" max="100" defaultValue="90" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="90" className="range w-80 md:w-96 range-primary" />
                 </div>
                 <div className=''>
                     <p className='text-xl mb-2'>Express</p>
-                    <input type="range" min="0" max="100" defaultValue="100" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="100" className="range w-80 md:w-96 range-primary" />
                 </div>
                 <div className=''>
                     <p className='text-xl mb-2'>Node JS</p>
-                    <input type="range" min="0" max="100" defaultValue="90" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="90" className="range w-80 md:w-96 range-primary" />
                 </div>
 
                 <div className=''>
                     <p className='text-xl mb-2'>Express JS</p>
-                    <input type="range" min="0" max="100" defaultValue="90" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="90" className="range w-80 md:w-96 range-primary" />
                 </div>
                 <div className=''>
                     <p className='text-xl mb-2'>MongoDB</p>
-                    <input type="range" min="0" max="100" defaultValue="60" className="range w-96 range-primary" />
+                    <input type="range" min="0" max="100" defaultValue="60" className="range w-80 md:w-96 range-primary" />
                 </div>
 
 
